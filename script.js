f2d498b264f0f3c9c43d72b53fb6b725c2407b33
Fejmi <fejmiqaz@gmail.com>
2025-01-18 18:26:30 +0100

    update_1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // SIDEBAR.JS
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     const sidebar = document.getElementById('sidebar');
     const toggleButton = document.getElementById('menu-toggle'); // Assuming there's a button with this ID
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
     let lastScrollTop = 0;
 
     // Toggle sidebar visibility
-    toggleButton.addEventListener('click', function() {
+    toggleButton.addEventListener('click', function () {
         sidebar.classList.toggle('open');
     });
 
     // Show/hide sidebar on scroll
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
         if (currentScroll > lastScrollTop) {
@@ -28,6 +28,18 @@ document.addEventListener('DOMContentLoaded', function() {
     //ABOUT SECTION SLIDE IN
     window.addEventListener('load', function () {
         const aboutSection = document.getElementById('home');
-        aboutSection.classList.add('show');
+        // aboutSection.classList.add('show');
+
+
+        let welcomeTo = document.getElementById('welcomeTo');
+        let metalorix = document.getElementById('metalorix');
+        let becomeBetter = document.getElementById('becomeBetter');
+
+        const elements = [metalorix, welcomeTo, becomeBetter];
+
+
+        elements.forEach((element, idx) => {
+            aboutSection.classList.add('show');
+        })
     })
 });
